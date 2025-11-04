f5f5122191e061663c5cef7f65496fe0234cd4cf
hoanmeo <khachoandz1@gmail.com>
2025-11-04 14:47:15 +0700

    lab1 bai1

---
+++ lab/lab1.js
@@ -0,0 +1,13 @@
+function tinhBmi (chieucao,cannang){
+ const bmi = cannang/(chieucao * chieucao);
+
+ const ketqua = 
+ bmi < 18.5 ? "Thieu can":
+ bmi < 25 ? "Binh thuong":
+ bmi < 30 ? "Thua can":
+ "Beo Phi";
+
+ return `BMI cua ban la ${bmi} (${ketqua})` ;
+
+}
+console.log(tinhBmi(1.76,92));
