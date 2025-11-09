9577f5d5717694f139c5a1f4b2eda33f235c8fa4
hoanmeo <khachoandz1@gmail.com>
2025-11-09 20:31:58 +0700

    update

--- lab/lab3.js
+++ lab/lab3.js
@@ -1,3 +1,4 @@
+//bai1
 const multiply=(a,b)=>a*b;
 
 const ispositive=(number)=>number = 0;
@@ -7,7 +8,7 @@ const getrandomnumber=()=> Math.random();
 document.addEventListener("click",()=>{
     console.log("clicked!");
 });
-
+//bai2
 function createuser(name = "anymous", age = 18, isadmin = false){
     return{
         name,
@@ -15,6 +16,7 @@ function createuser(name = "anymous", age = 18, isadmin = false){
         isadmin,
     };
 }
+//bai4
 
 const shoppingCart = function(customerName) {
   const products = [];
@@ -36,4 +38,47 @@ const shoppingCart = function(customerName) {
 console.log(shoppingCart("Hoàn", "Áo thun", "Quần jean", "Giày thể thao"));
 console.log(shoppingCart("Linh", "Bánh", "Nước ngọt"));
 console.log(shoppingCart());
+
+
+
+//bai 3
+const mergeArrays = function() {
+  const result = [];
+  for (let i = 0; i < arguments.length; i++) {
+    const currentArray = arguments[i];
+    if (Array.isArray(currentArray)) {
+      for (let j = 0; j < currentArray.length; j++) {
+        result.push(currentArray[j]);
+      }
+    }
+  }
+  return result;
+};
+
+const sumAll = function() {
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    const number = Number(arguments[i]);
+    if (!isNaN(number)) total += number;
+  }
+  return total;
+};
+
+const createProduct = function(name, price, category) {
+  if (!name) name = "Sản phẩm mới";
+  if (!price) price = 0;
+  if (!category) category = "Chưa phân loại";
+
+  return {
+    name: name,
+    price: price,
+    category: category
+  };
+};
+
+// Ví dụ sử dụng
+console.log(mergeArrays([1, 2], [3], [4, 5]));
+console.log(sumAll(1, 2, 3, 4));
+console.log(createProduct("Áo thun", 150000, "Thời trang"));
+console.log(createProduct());
 
